1c34fe4703f25b46269ddf51db6384fc9e2c9de2
bolortuya tumurbaatar <btumurbaatar@hawk.iit.edu>
2019-11-13 23:01:56 -0600

    fix validate issue

---
+++ js/jscript.js
@@ -0,0 +1,25 @@
+function init() {
+    var button = document.getElementById('btn');
+	  var email = document.getElementById("email");
+
+  if(firstname.value == "") {
+		alert("Please enter your first name!");
+		return false;
+	}
+  if(lastname.value == "") {
+		alert("Please enter your last name!");
+		return false;
+	}
+  if(email.value == "") {
+		alert("Please enter your email!");
+		return false;
+	}
+  if(subject.value == "") {
+		alert("Please leave a message!");
+		return false;
+	}
+  else {
+		alert("Message sent!");
+		return true;
+	}
+}
