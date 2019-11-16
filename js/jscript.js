90019cfd52654e70e86287cfb6b29fe7723254a6
bolortuya tumurbaatar <btumurbaatar@hawk.iit.edu>
2019-11-15 20:44:30 -0600

    add e.preventDefault();

--- js/jscript.js
+++ js/jscript.js
@@ -1,26 +1,29 @@
-function init() {
-    var button = document.getElementById('btn');
-	  var email = document.getElementById("email");
+function init(e) {
+  var button = document.getElementById('btn');
+	var email = document.getElementById("email");
 
   if(firstname.value == "") {
 		alert("Please enter your first name!");
+    e.preventDefault();
 		return false;
 	}
   if(lastname.value == "") {
 		alert("Please enter your last name!");
+    e.preventDefault();
 		return false;
 	}
   if(email.value == "") {
 		alert("Please enter your email!");
+    e.preventDefault();
 		return false;
 	}
   if(subject.value == "") {
 		alert("Please leave a message!");
+    e.preventDefault();
 		return false;
 	}
   else {
 		alert("Message sent!");
 		return true;
-    e.preventDefault();
 	}
 }
